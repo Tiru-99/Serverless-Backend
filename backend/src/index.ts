c902f5d76dd986ad9ee68955906af89afa145b9d
Tiru-99 <aayushtirmanwar1234@gmail.com>
2024-06-07 09:47:26 +0530

    user side backend done

---
+++ backend/src/index.ts
@@ -0,0 +1,115 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign , verify} from 'hono/jwt'
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET : string
+  
+  }
+}>()
+
+app.use('/api/v1/blog/*', async(c,next)=>{
+  //get the authorization header
+  const header = c.req.header("authorization") || ""
+
+  //Bearer token => ["Bearer" , "Token"]
+  //as we provide token as Bearer then auth token
+  const token = header.split(" ")[1];
+
+  if(!header){
+    return c.json({
+      msg : "no header found"
+    })
+  }
+  //verify the authorization header 
+  const response  = await verify(header, c.env.JWT_SECRET)
+  if(response.id){
+    next();
+  }
+  else{
+    return c.json({
+      msg:"unauthorized error"
+    })
+  }
+  //if header is correct, we can proceed 
+  //if not , we return a status code of 403
+})
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/signup' , async(c)=>{
+
+  const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+  const body = await c.req.json(); 
+
+  try{
+
+    const existingUser = await prisma.user.findUnique({
+      where:{
+        email : body.email
+      }
+    })
+
+    if(existingUser){
+      return c.json({
+        "msg":"User already exists"
+      })
+    }
+
+    else{
+    const user = await prisma.user.create({
+      data :{
+        email : body.email,
+        password : body.password
+      }
+    });
+    
+    console.log("user created successfully");
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+		return c.json({ jwt : jwt});
+
+    
+  }
+ } catch(e){
+    console.log("There is something really wrong", e);
+  }
+
+});
+
+app.post('/api/v1/signin',async(c)=>{
+  const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+  const body = await c.req.json(); 
+
+  const user = await prisma.user.findUnique({
+    where:{
+      email : body.email, 
+      password : body.password,
+    }
+  });
+
+  if(!user){
+    return c.json({
+      "msg" : "User not found"
+    })
+  }
+
+  const jwt = await sign({id:user.id } , c.env.JWT_SECRET);
+  return c.json({
+    "jwt":jwt
+  });
+
+})
+
+
+export default app
